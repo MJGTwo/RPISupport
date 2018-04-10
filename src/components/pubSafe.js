5251bc6c59e112b876fc631a64b646d0a06755ec
Michael Gardner <gardnm4@rpi.edu>
2018-04-10 15:07:59 -0400

    fixed listing bug

--- src/components/pubSafe.js
+++ src/components/pubSafe.js
@@ -6,33 +6,32 @@ const pubSafe = () => {
     <Card
       title={'RPI Public Safety'}
       text={[
-        `Communication, awareness, and prevention are essential for personal
-        safety. At Rensselaer, we take a proactive approach to maintaining a
-        safe and secure environment for all.`,
-        `The department also provides 24-hour per day, 365-day response to
-        life-safety incidents, coordinates internal and external resources to
-        assist the
-        ${(
+        <p>
+          Communication, awareness, and prevention are essential for personal
+          safety. At Rensselaer, we take a proactive approach to maintaining a
+          safe and secure environment for all.
+        </p>,
+        <p>
+          The department also provides 24-hour per day, 365-day response to
+          life-safety incidents, coordinates internal and external resources to
+          assist the{' '}
           <a
             href="http://www.rpi.edu/dept/public_safety/emergency/index.html"
             style={{ color: GREY }}
           >
             management of critical incidents
           </a>
-        )}
-
-        , and conducts
-        ${(
+          , and conducts
           <a
             href="http://www.rpi.edu/dept/public_safety/safety/personal.html"
             style={{ color: GREY }}
           >
             {' '}
             awareness programs
-          </a>
-        )}
-        to proactively assist its community members in the maintenance of
-        personal safety.`
+          </a>{' '}
+          to proactively assist its community members in the maintenance of
+          personal safety.
+        </p>
       ]}
       url="http://www.rpi.edu/dept/public_safety/"
     />
