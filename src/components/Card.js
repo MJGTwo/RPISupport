014cf5d3a4edbb93a49b7cd546ff75c6e5b67244
Michael Gardner <gardnm4@rpi.edu>
2018-04-14 00:20:16 -0400

    separated the function from the class

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,27 @@ import React from 'react';
 const RED = '#97040c';
 const GREY = '#717271';
 const Card = ({ title, text, url }) => {
+  const linkHelper = path => {
+    if (typeof path.name !== 'undefined') {
+      return (
+        <a
+          target="_blank"
+          style={{ color: GREY, fontWeight: 'bold' }}
+          download={path.name}
+          href={path.file}
+        >
+          {path.name}
+        </a>
+      );
+    } else {
+      return (
+        <a href={path} style={{ color: GREY, fontWeight: 'bold' }}>
+          Click for website
+        </a>
+      );
+    }
+  };
+
   return (
     <div className="card" style={{ background: RED }}>
       <div className="card-content white-text">
@@ -12,24 +33,7 @@ const Card = ({ title, text, url }) => {
       </div>
       <div className="card-action" style={{ background: 'white' }}>
         {url.map(path => {
-          if (typeof path.name !== 'undefined') {
-            return (
-              <a
-                target="_blank"
-                style={{ color: GREY, fontWeight: 'bold' }}
-                download={path.name}
-                href={path.file}
-              >
-                {path.name}
-              </a>
-            );
-          } else {
-            return (
-              <a href={path} style={{ color: GREY, fontWeight: 'bold' }}>
-                Click for website
-              </a>
-            );
-          }
+          return linkHelper(path);
         })}
       </div>
     </div>
